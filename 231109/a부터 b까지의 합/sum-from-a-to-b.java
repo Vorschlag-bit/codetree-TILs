65958471dd2d68cdf06c0249dd2173efa918dc74
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-09 23:20:59 +0900

    Title: a부터 b까지의 합, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231109/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc =new Scanner(System.in);
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        long sum = 0;
+        for(long i =a;i<=b;i++){
+            sum+=i;
+        }
+        System.out.print(sum);
+    }
+}
