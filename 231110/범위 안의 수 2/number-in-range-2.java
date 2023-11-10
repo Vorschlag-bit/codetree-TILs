912668066526a8c5cfda5dd4678bc725ba1bc628
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-10 13:40:59 +0900

    Title: 범위 안의 수 2, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 231110/범위 안의 수 2/number-in-range-2.java
+++ 231110/범위 안의 수 2/number-in-range-2.java
@@ -3,18 +3,17 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        long n = sc.nextInt();
         long sum=0;
         long cnt=0;
         double avg;
-        for(long i =1;i<=n;i++){
+        for(long i =1;i<=10;i++){
             long a = sc.nextInt();
             if(a>=0&&a<=200){
                 sum+=a;
-                cnt+=i;
-                double avg = (double)sum/cnt;
+                cnt+=1;
             }
         }
+        avg = (double)sum/cnt;
         System.out.printf("%d %.1f", sum, avg);
     }
 }
