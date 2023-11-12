f054b82bf3b971c451983046f3e1db3d8e715871
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-12 15:48:40 +0900

    Title: 3n + 1 수열, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 231112/3n + 1 수열/3n-plus-1-sequence.java
+++ 231112/3n + 1 수열/3n-plus-1-sequence.java
@@ -6,6 +6,10 @@ public class Main {
         long cnt=0;
         long n =sc.nextInt();
         while(true){
+            if(n==1){
+                System.out.print(cnt);
+                break;
+            }
             if(n%2!=0){
                 n=n*3+1;
             }
@@ -13,10 +17,6 @@ public class Main {
                 n/=2;
             }
             cnt+=1;
-            if(n==1){
-                System.out.print(cnt);
-                break;
-            }
         }
     }
 }
