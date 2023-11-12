8aea5715cbde5741ceda34d19fa29936d20a0c42
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-12 13:28:20 +0900

    Title: 특정 숫자 맞추기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231112/특정 숫자 맞추기/catching-specific-number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            long n = sc.nextLong();
+            if(n<25){
+                System.out.println("Higher");
+            }
+            else if(n>25){
+                System.out.println("Lower");
+            }
+            if(n==25){
+                System.out.println("Good");
+                break;
+            }
+        }
+    }
+}
