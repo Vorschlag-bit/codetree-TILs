1c661a53b1d788ef3572a7ede95a87143a3032d2
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-12 16:30:46 +0900

    Title: 합성수 판별기, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231112/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        boolean state =false;
+        for(long i =1;i<=n;i++){
+            if(n%i==0&&i>1){
+                state = true;
+            }
+            }
+            if(state==true){
+                System.out.print("C");
+            }
+            else System.out.print("N");
+        }
+    }
