ad0b06365d06b8a2d7fb47cad70665a871489d6b
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-12 13:48:04 +0900

    Title: 몫 출력하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231112/몫 출력하기/print-share.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long cnt=0;
+        while(true){
+        long n = sc.nextInt();
+        if(n%2==1){
+            continue;
+        }
+        else if(n%2==0){
+            System.out.println(n/2);
+            cnt++;
+            if(cnt==3){
+                break;
+            }
+        }
+        }
+    }
+}
