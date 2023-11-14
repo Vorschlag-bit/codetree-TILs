019b69e8455c4131c8b0509ec5269a028a1ae390
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-14 23:51:57 +0900

    Title: 직각 삼각형 출력, Time: 3011ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

---
+++ 231114/직각 삼각형 출력/Right-triangle-and-output.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        for(long i =1; i<=n;i++){
+            for(long j = 1;j<=i;j++){
+                for(long a = 1;a<=2*a-1;a++){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
