26e72bffc7e5574ed4d0cb5e95bee0761535454c
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-16 22:18:52 +0900

    Title: 별표 출력하기 6, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231116/별표 출력하기 6/print-star-6.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        for(long i =0;i<n;i++){
+            for(long j = 0;j<i;j++){
+                System.out.print("  ");
+            }
+            for(long j = 0;j<2*n-1-2*i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(long i = 0;i<n-1;i++){
+            for(long j = n-2-i;j>0;j--){
+                System.out.print("  ");
+            }
+            for(long j = 0;j<3+2*i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
