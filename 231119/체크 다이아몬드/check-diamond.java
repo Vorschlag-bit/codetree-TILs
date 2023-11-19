41bb023d28fbfbcb5d7546fb3c3ed4db76d61bd9
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-19 13:07:53 +0900

    Title: 체크 다이아몬드, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231119/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        for(long i = 1;i<=n;i++){
+            for(long j = 1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(long j = 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(long i = 1;i<=n-1;i++){
+            for(long j = 1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(long j = n-i;j>=1;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
