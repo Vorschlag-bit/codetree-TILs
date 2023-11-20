02de73d384d343ad956c5f5886e2c95061bca226
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-20 22:16:55 +0900

    Title: 규칙을 찾아 출력하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231120/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        for(long i = 0; i<n; i++){
+            for(long j = 0; j<n;j++){
+                if(i==0||j==0||i==n-1||j==n-1||i>j)
+                System.out.print("* ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
