78483fda78e10a3487e5e3e095ae253881c8ab8e
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-21 22:52:51 +0900

    Title: 숫자 피라미드 2, Time: 161ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231121/숫자 피라미드 2/number-pyramid-2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long cnt = 1;
+        for(long i = 1;i<=n;i++){
+            for(long j = 0;j<i;j++){
+                System.out.print(cnt+" ");
+                cnt++;
+            }
+            System.out.println();
+        }
+    }
+}
