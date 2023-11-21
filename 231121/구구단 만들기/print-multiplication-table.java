8399a6ed4fe7ccac88ad1425aca2e85fb061738c
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-21 16:52:48 +0900

    Title: 구구단 만들기, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 231121/구구단 만들기/print-multiplication-table.java
+++ 231121/구구단 만들기/print-multiplication-table.java
@@ -5,10 +5,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         long a = sc.nextLong();
         long b = sc.nextLong();
-        for(long i =1;i<10;i++){
-            for(long j =1;j<b/a+1;j++){
-                System.out.print((b-2*(j-1))+" * "+i+" = "+(b-2*(j-1))*i);
-                if(j<b/a)
+        for(long i = 1;i<10;i++){
+            for(long j = 0;j<(b-a)/2+1;j++){
+                System.out.print(b-2*j+" * "+i+" = "+(b-2*j)*i);
+                if(j<(b-a)/2)
                 System.out.print(" / ");
             }
             System.out.println();
