c5b14d2f0c05025c8d73493f9df79c5886b0cfed
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-22 16:07:19 +0900

    Title: 특이한 출력, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231122/특이한 출력/special-print.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        for(long  i = 1; i<=n; i++){
+            for(long j =1; j<=n; j++){
+                if((i+j)%4==0)
+                System.out.println("("+i+", "+j+")");
+                else
+                System.out.print("("+i+", "+j+")"+" ");
+            }
+        }
+    }
+}
