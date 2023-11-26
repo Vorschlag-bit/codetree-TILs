40a400b0a32110009a52bf1914db791deb2be079
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-26 23:47:22 +0900

    Title: 점수대 파악하기, Time: 240ms, Memory: 10MB, Status: Passed - CodeTree

--- 231126/점수대 파악하기/find-out-the-score-range.java
+++ 231126/점수대 파악하기/find-out-the-score-range.java
@@ -4,18 +4,18 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         
-        int[] arr = new int[101];
-        int[] arrcnt = new int[101];
+        int[] arr = new int[100];
+        int[] arrcnt = new int[11];
 
-        for(int i = 0; i<101; i++){
+        for(int i = 0; i<100; i++){
             arr[i] = sc.nextInt();
             if(arr[i]==0)
             break;
             if(arr[i]<10)
             continue;
-            arrcnt[arr[i]/10*10]++;
+            arrcnt[arr[i]/10]++;
         }
         for(int i = 100; i>=10; i-=10)
-        System.out.println(i+" - "+arrcnt[i]);
+        System.out.println(i+" - "+arrcnt[i/10]);
     }
 }
