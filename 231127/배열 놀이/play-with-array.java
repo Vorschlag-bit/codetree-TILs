657de992d641a88c512208743fdd258fe9a12254
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-27 14:23:08 +0900

    Title: 배열 놀이, Time: 140ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231127/배열 놀이/play-with-array.java
+++ 231127/배열 놀이/play-with-array.java
@@ -1,43 +1,44 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int q = sc.nextInt();
-        int[] arr = new int[n + 1];
+        int[] arr = new int[n+1];
         int idx = -1;
 
-        for (int i = 1; i <= n; i++) {
+        for(int i = 1; i<=n; i++){
             arr[i] = sc.nextInt();
         }
 
-        for (int i = 1; i <= q; i++) {
+        for(int i = 1; i<=q; i++){
             int a = sc.nextInt();
-            if (a == 3) {
+            if(a==3){
                 int b = sc.nextInt();
                 int c = sc.nextInt();
-                for (int j = b; j <= c; j++) {
-                    System.out.print(arr[j] + " ");
+                for(int j = b; j<=c; j++){
+                    System.out.print(arr[j]+" ");
                 }
                 System.out.println();
-            } else {
-                int b = sc.nextInt();
-                if (a == 1) {
-                    System.out.println(arr[b]);
-                } else if (a == 2) {
-                    idx = -1;
-                    for (int j = 1; j <= n; j++) {
-                        if (b == arr[j]) {
-                            idx = j;
-                            break;
-                        }
-                    }
-                    if (idx == -1)
-                        System.out.println(0);
+            }
+            else{
+            int b = sc.nextInt();
+            if(a==1)
+            System.out.println(arr[b]);
+
+            else if(a==2){
+                for(int j = 1; j<=n; j++){
+                    if(b==arr[j]){
+                        idx=j;
+                        break;
+                    }}
+                    if(idx==-1)
+                    System.out.println(0);
                     else
-                        System.out.println(idx);
-                }
+                    System.out.println(idx);
+                
+            }
             }
         }
     }
