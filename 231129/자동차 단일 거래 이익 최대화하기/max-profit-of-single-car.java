c7c672af1a3b60bde69fa36656e338080738190b
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-29 23:06:09 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 208ms, Memory: 13MB, Status: Passed - CodeTree

--- 231129/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 231129/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -8,28 +8,20 @@ public class Main {
         int n = sc.nextInt();
 
         int[] arr = new int[1000];
-        int[] b = new int[1000];
 
         for(int i = 0; i<n; i++)
         arr[i] = sc.nextInt();
 
-        int gap = -1;
+        int Maxgap = 0;
 
         for(int i = 0; i<n; i++){
             for(int j = i; j<n; j++){
-                if(arr[j]-arr[i]>gap)
-                gap = arr[j] - arr[i];
+                int gap = arr[j] - arr[i];
+                
+                if(gap>Maxgap)
+                Maxgap = gap;
             }
-            b[i] = gap; 
         }
-        int Maxgap = b[0];
-
-        for(int i = 0; i<b.length; i++){
-            if(b[i]>=Maxgap)
-            Maxgap = b[i];
-        }
-
-        if(gap == -1) System.out.print(0);
-        else System.out.print(Maxgap);
+        System.out.print(Maxgap);
     }
 }
