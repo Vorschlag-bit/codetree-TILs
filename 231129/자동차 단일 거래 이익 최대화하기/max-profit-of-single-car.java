73adda0df490ecc545aee19249acdccd6d4c4505
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-29 23:01:57 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 204ms, Memory: 13MB, Status: Passed - CodeTree

--- 231129/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 231129/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -17,7 +17,7 @@ public class Main {
 
         for(int i = 0; i<n; i++){
             for(int j = i; j<n; j++){
-                if(arr[j]>arr[i])
+                if(arr[j]-arr[i]>gap)
                 gap = arr[j] - arr[i];
             }
             b[i] = gap; 
