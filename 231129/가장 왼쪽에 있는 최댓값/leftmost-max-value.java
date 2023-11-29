5a58a4ddfd45f65bda85c36a130084e97a66b5ef
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-29 21:48:59 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 202ms, Memory: 13MB, Status: Passed - CodeTree

--- 231129/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 231129/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -5,24 +5,28 @@ public class Main {
         Scanner sc= new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] idx = new int[1000]; //arr배열의 index를 담아두는 배열
+
         int[] arr = new int[1000]; 
-        int cnt = 0; //index 배열의 크기 측정
 
         for(int i = 0; i<n; i++)
             arr[i] = sc.nextInt();
-
-            idx[cnt++] = 0; //arr의 첫 번째 원소는 마지막에 출력되므로
         
-        for(int i = 0; i<n; i++){
-               int lastidx = idx[cnt-1]; //비교를 하기 위한 가장 최근 idx값 지정
-               if(arr[i]>arr[lastidx])
-               idx[cnt++] = i; //마지막 idx보다 크다면 그때의 i값을 idx에 저장
-            }
+        int preMaxidx = n; //가장 처음 탐색할 때는 최대크기까지 훑기
 
-            for(int i = cnt - 1; i>=0; i--){
-                int index = idx[i]; //내림차순으로 정렬
-                System.out.print((index+1)+" ");//idx 마지막이 1이 되야하므로
-            }
+        while(true){
+
+            int maxidx = 0; //시작은 항상 왼쪽(= 첫 번째)부터
+
+            for(int i = 0; i<preMaxidx; i++)
+                if(arr[i]>arr[maxidx])
+                maxidx = i;
+            System.out.print((maxidx+1)+" ");
+
+            if(maxidx == 0)
+            break;
+
+            preMaxidx = maxidx;
+            
+        }
     }
 }
