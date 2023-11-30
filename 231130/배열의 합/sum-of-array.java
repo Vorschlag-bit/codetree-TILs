fa68a00340f14abef6697c489475ab6b873e19eb
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-11-30 22:00:04 +0900

    Title: 배열의 합, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231130/배열의 합/sum-of-array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new  Scanner(System.in);
+
+        int [][] arr = new int[4][4];
+
+        for(int i = 0; i<4; i++){
+            int sum = 0;
+            for(int j = 0; j<4; j++){
+                int val = sc.nextInt();
+                sum += val;
+            }
+            System.out.println(sum);
+        }
+
+    }
+}
