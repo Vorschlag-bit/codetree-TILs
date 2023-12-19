a84eeb2dd29409f2cfffe83b9db99dedc1050c8f
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-19 23:38:30 +0900

    Title: 문자 개수 세기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231219/문자 개수 세기/count-char.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        char[] Arr = str.toCharArray();
+
+        int cnt = 0;
+
+        for(int i = 0; i<Arr.length; i++){
+            if(Arr[i]==str2.charAt(0))
+            cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
