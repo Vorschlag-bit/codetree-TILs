62417604e8aa51b0056ef670a2e0db5c5e67e03d
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-19 23:19:59 +0900

    Title: 가장 짧은 문자열, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 231219/가장 짧은 문자열/shortest-string.java
+++ 231219/가장 짧은 문자열/shortest-string.java
@@ -12,22 +12,22 @@ public class Main {
         int len2 = str2.length();
         int len3 = str3.length();
 
-        if(len1>len2&&len2>len3){
+        if(len1>len2&&len2>=len3){
             System.out.println(len1 - len3);
         }
-        else if(len1>len3&&len3>len2){
+        else if(len1>len3&&len3>=len2){
             System.out.println(len1-len2);
         }
-        else if(len2>len1&&len1>len3){
+        else if(len2>len1&&len1>=len3){
             System.out.println(len2-len3);
         }
-        else if(len2>len3&&len3>len1){
+        else if(len2>len3&&len3>=len1){
             System.out.println(len2-len1);
         }
-        else if(len3>len2&&len2>len1){
+        else if(len3>len2&&len2>=len1){
             System.out.println(len3-len1);
         }
-        else if(len3>len1&&len1>len2){
+        else if(len3>len1&&len1>=len2){
             System.out.println(len3-len2);
         }
     }
