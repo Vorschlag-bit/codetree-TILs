bad3ed7475e5e2852a9d220bcd3d498e71e7fda7
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-25 18:00:53 +0900

    Title: 문자열 밀기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231225/문자열 밀기/push-char.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        int len = s.length();
+
+        s = s.substring(1,len) + s.substring(0,1);
+
+        System.out.println(s);
+    }
+}
