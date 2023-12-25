4216cf3a124b5c471768b014da83c25b03595370
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-25 21:26:22 +0900

    Title: 그 다음 알파벳, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231225/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        char a = sc.next().charAt(0);
+
+        int b = (int) a;
+
+        if(a=='z')
+        System.out.print('a');
+        else
+        System.out.print((char)(b+1));
+    }
+}
