b5b6ca93d34b7a0fe92b0861e65cc79123c43be9
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-27 23:15:39 +0900

    Title: 알파벳 지우기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231227/알파벳 지우기/remove-alphabet.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.next();
+        String s2 = sc.next();
+
+        String a = "";
+        String b = "";
+
+        for(int i = 0; i<s1.length(); i++){
+            if(s1.charAt(i)>='0'&&s1.charAt(i)<='9'){
+                a += s1.charAt(i);
+            }
+        }
+
+        for(int i = 0; i<s2.length(); i++){
+            if(s2.charAt(i)>='0'&&s2.charAt(i)<='9'){
+                b += s2.charAt(i);
+            }
+        }
+
+        int ia = Integer.parseInt(a);
+        int ib = Integer.parseInt(b);
+
+        System.out.println(ia+ib);
+    }
+}
