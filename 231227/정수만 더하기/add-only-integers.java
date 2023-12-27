e4ddc33b1fc87dcfa020a71ffb9876e205539bc6
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-27 22:20:40 +0900

    Title: 정수만 더하기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231227/정수만 더하기/add-only-integers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.next();
+
+        int sum = 0;
+
+        for(int i = 0; i<A.length(); i++){
+            if(A.charAt(i)>='0'&&A.charAt(i)<='9'){
+                sum += A.charAt(i)-48;
+            }
+        }
+        System.out.println(sum);
+    }
+}
