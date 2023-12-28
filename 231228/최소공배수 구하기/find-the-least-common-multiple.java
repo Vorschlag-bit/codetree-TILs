db73ffb62f82a5d7305121d73b4c50f2cc05c2ea
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-28 22:44:28 +0900

    Title: 최소공배수 구하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 231228/최소공배수 구하기/find-the-least-common-multiple.java
+++ 231228/최소공배수 구하기/find-the-least-common-multiple.java
@@ -1,7 +1,20 @@
 import java.util.*;
 public class Main {
     public static void com(int n, int m){
-        
+        int max = Integer.max(n,m);
+
+        int g = 0;
+
+        for(int i = 1; i<=max; i++){
+            if(n%i==0&&m%i==0)
+            g = i;
+        }
+
+        int ex1 = n/g;
+        int ex2 = m/g;
+
+        System.out.println(ex1*ex2*g);
+
     }
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
