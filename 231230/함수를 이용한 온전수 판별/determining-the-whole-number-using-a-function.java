a3dcd74ff785cb6ffba6233148a9ba6af7bb7cd3
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-30 23:41:24 +0900

    Title: 함수를 이용한 온전수 판별, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231230/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static boolean check(int n){
+        if(n%2==0)
+        return false;
+        if(n%10==5)
+        return false;
+        if(n%3==0&&n%9!=0)
+        return false;
+
+        return true;
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int cnt = 0;
+
+        for(int i =a; i<=b; i++){
+            if(check(i)==true)
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
