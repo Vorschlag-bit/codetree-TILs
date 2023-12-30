f8fc2d5e75750fe25b05ba6bb3c6091395b8eddb
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-30 22:21:08 +0900

    Title: 함수를 이용한 윤년 판별, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231230/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static boolean year(int n){
+        if(n%4!=0)
+        return false;
+        if(n%4==0&&n%100==0)
+        return false;
+        if(n%4!=0&&n%100!=0&&n%400!=0)
+        return false;
+
+        return true;
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+
+        if(year(y))
+        System.out.println("true");
+        else
+        System.out.println("false");
+    }
+}
