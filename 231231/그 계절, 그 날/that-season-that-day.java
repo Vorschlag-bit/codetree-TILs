7b5238a066589e6ee23e52b48557ccae3859c1bb
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-31 01:44:57 +0900

    Title: 그 계절, 그 날, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 231231/그 계절, 그 날/that-season-that-day.java
+++ 231231/그 계절, 그 날/that-season-that-day.java
@@ -1,54 +1,58 @@
 import java.util.*;
+
 public class Main {
-    public static boolean Yun(int y){
-        if(y%4!=0)
-        return false;
-        if(y%100!=0)
-        return true;
-        if(y%400!=0)
-        return false;
+    public static boolean Yun(int y) {
+        if (y % 4 != 0)
+            return false;
+        if (y % 100 == 0 && y % 400 != 0)
+            return false;
 
         return true;
     }
-    public static boolean Daycheck(int y,int m, int d){
-        if(Yun(y)&&m==2){
-            if(d<=29)
-            return true;
+
+    public static boolean Daycheck(int y, int m, int d) {
+        if (Yun(y) && m == 2) {
+            if (d <= 29)
+                return true;
+        } else {
+            if (m == 2) {
+                if (d <= 28)
+                    return true;
+            } else if (m == 4 || m == 6 || m == 9 || m == 11) {
+                if (d <= 30)
+                    return true;
+            } else {
+                if (d <= 31)
+                    return true;
+            }
         }
-        else if(m==2)
-        if(d<=28)
-        return true;
-        else if(m==4||m==6||m==9||m==11)
-        if(d<=30)
-        return true;
-        else
-        if(d<=31)
-        return true;
+        return false;
+    }
 
-        return false;
+    public static String season(int m) {
+        if (m == 12 || m <= 2)
+            return "Winter";
+        if (m >= 3 && m <= 5)
+            return "Spring";
+        if (m >= 6 && m <= 8)
+            return "Summer";
+        if (m >= 9 && m <= 11)
+            return "Fall";
+
+        return "Invalid month";
     }
-    public static boolean season(int m){
-        if(m==12||m<=2)
-        System.out.println("Winter");
-        if(m>=3&&m<=5)
-        System.out.println("Spring");
-        if(m>=6&&m<=8)
-        System.out.println("Summer");
-        if(m>=9&&m<=11)
-        System.out.println("Fall");
-        return false;
-    }
+
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
         int y = sc.nextInt();
         int m = sc.nextInt();
         int d = sc.nextInt();
 
-        if(Daycheck(y,m,d))
-        season(m);
-        else
-        System.out.println(-1);
+        if (Daycheck(y, m, d)) {
+            System.out.println(season(m));
+        } else {
+            System.out.println(-1);
+        }
     }
 }
