343ff14a6495ba3f1b90c30fa061c563128465db
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2023-12-30 22:12:56 +0900

    Title: 함수를 이용한 소수 판별, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231230/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+    public static boolean prime(int n){
+        for(int i = 2; i<n; i++){
+            if(n%i==0)
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = 0;
+
+        for(int i = a; i<=b; i++){
+            if(prime(i))
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
