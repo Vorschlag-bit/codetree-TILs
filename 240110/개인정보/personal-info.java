c1817e1b7cf51a9566009f1184a516976e705676
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-01-10 17:58:44 +0900

    Title: 개인정보, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240110/개인정보/personal-info.java
+++ 240110/개인정보/personal-info.java
@@ -14,24 +14,35 @@ class Person implements Comparable<Person> {
 
     @Override
     public int compareTo(Person person) {
-        return (person.height - this.height)&&(this.name - person.name);
+        return person.height - this.height;
     }
 }
 public class Main {
     public static void main(String[] args)throws IOException {
         // 여기에 코드를 작성해주세요.
-        BufferReader br = new BufferReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Person[] persons = new Person[5];
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < 5; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             persons[i] = new Person(st.nextToken(), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
         Arrays.sort(persons);
 
-        System.out.println(name);
-        for(int i = 0; i < n; i++) {
+        Arrays.sort(persons, new Comparator<Person>() {
+            @Override
+            public String compare(Person a, Person b) {
+                return a.name.compareTo(b.name);
+            }
+        });
+
+        System.out.println("name");
+        for(int i = 0; i < 5; i++) {
+            System.out.println(persons[i].name+" "+persons[i].height+" "+persons[i].weight);
+        }
+        System.out.println("height");
+        for(int i = 0; i < 5; i++) {
             System.out.println(persons[i].name+" "+persons[i].height+" "+persons[i].weight);
         }
     }
