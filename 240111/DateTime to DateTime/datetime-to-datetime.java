3a2924d6c152c534742288f9d14246abd8bd8595
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-01-11 13:50:53 +0900

    Title: DateTime to DateTime, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240111/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static int Cal(int a, int b, int c) {
+        return (24 * 60 * a + 60 * b + c) - (11 * 24 * 60 + 60 * 11 + 11); 
+    }
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if(a < 11 || b < 11 || c < 11)
+        System.out.print(-1);
+
+        System.out.print(Cal(a, b, c));
+    }
+}
