70daa1ae47b25105987fac8712def315a8664751
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-01-21 17:39:30 +0900

    Title: Date to Date, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240121/Date to Date/date-to-date.java
+++ 240121/Date to Date/date-to-date.java
@@ -1,7 +1,17 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static int[] dsom = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static int cal(int m, int d) {
+        int[] dsom = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int Totalday = 0;
+
+        for(int i = 1; i < m; i++)
+        Totalday += dsom[i];
+
+        Totalday += d;
+
+        return Totalday;
+    }
     public static void main(String[] args)throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,19 +23,6 @@ public class Main {
         int m2 = Integer.parseInt(st.nextToken());
         int d2 = Integer.parseInt(st.nextToken());
         
-        int TotalDay = 0;
-        
-        while(!(m1==m2&&d1==d2)){
-
-            TotalDay++;
-            d1++;
-
-            if(d1 > dsom[m1]){
-                m1++;
-                d1 = 1;
-            }
-        }
-
-        System.out.print(TotalDay + 1);
+        System.out.print(cal(m2, d2) - cal(m1, d1) + 1);
     }
 }
