9a63118f3bba52ae7b4c0dec4cb32d7c91172fb5
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-01-21 23:47:55 +0900

    Title: 진수 to 진수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240121/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        String n = br.readLine();
+        int num = 0;
+
+        for(int i = 0; i < n.length(); i++) {
+            num = num * a + (n.charAt(i) - '0');
+        }
+
+        int cnt = 0;
+        int[] list = new int[10];
+
+        while(true) {
+            if(num < b){
+                list[cnt++] = num;
+                break;
+            }
+
+            list[cnt++] = num%b;
+            num /= b;
+        }
+
+        for(int i = cnt - 1; i >= 0; i--) {
+            System.out.print(list[i]);
+        }
+    }
+}
