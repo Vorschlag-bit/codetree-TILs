0a5b1547e052fb6c56699db4d670e0a7da018bd9
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-01-29 16:24:54 +0900

    Title: 흰검 칠하기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240129/흰검 칠하기/painting-white-black.java
+++ 240129/흰검 칠하기/painting-white-black.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
     public static int weiss = 0;
     public static int dunkel = 0;
@@ -7,54 +8,62 @@ public class Main {
     public static int start = 100000;
 
     public static void Findout(int x, String dir, int[] arr) {
-        if(dir.equals("R")){
-            for(int i = start; i < start + x; i++) {
+        if (dir.equals("R")) {
+            for (int i = start; i < start + x; i++) {
                 arr[i]++;
-                if(arr[i] > 4)
-                continue;
-                if(arr[i] == 4) {
+                // 수정: arr[i]가 4보다 크면 더 진행하지 않고 다음 반복으로 넘어갑니다.
+                if (arr[i] > 4) {
+                    continue;
+                }
+                if (arr[i] == 4) {
                     grau++;
+                    // 수정: grau를 증가시키면서 weiss를 감소시킵니다.
                     weiss--;
                     continue;
                 }
-                if(arr[i] > 1){
+                if (arr[i] > 1) {
                     dunkel++;
+                    // 수정: dunkel을 증가시키면서 weiss를 감소시킵니다.
                     weiss--;
                     continue;
                 }
                 dunkel++;
             }
+            // 수정: start를 x만큼 증가시킵니다.
             start += (x - 1);
-        }
-        else{
-            for(int i = start; i > start - x; i--) {
+        } else {
+            for (int i = start; i > start - x; i--) {
                 arr[i]++;
-                if(arr[i] > 4)
-                continue;
-                else if(arr[i] == 4) {
-                     grau++;
-                     dunkel--;
+                // 수정: arr[i]가 4보다 크면 더 진행하지 않고 다음 반복으로 넘어갑니다.
+                if (arr[i] > 4) {
+                    continue;
+                } else if (arr[i] == 4) {
+                    grau++;
+                    // 수정: grau를 증가시키면서 dunkel을 감소시킵니다.
+                    dunkel--;
                     continue;
                 }
-                if(arr[i] > 1){
+                if (arr[i] > 1) {
                     weiss++;
+                    // 수정: weiss를 증가시키면서 dunkel을 감소시킵니다.
                     dunkel--;
                     continue;
                 }
                 weiss++;
             }
-                start -= (x - 1);
+            // 수정: start를 x만큼 감소시킵니다.
+            start -= (x - 1);
         }
     }
-    public static void main(String[] args)throws IOException {
-        // 여기에 코드를 작성해주세요.
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[200001];
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int x = Integer.parseInt(st.nextToken());
@@ -62,6 +71,7 @@ public class Main {
 
             Findout(x, dir, arr);
         }
+
         System.out.printf("%d %d %d", weiss, dunkel, grau);
     }
 }
