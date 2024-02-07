c1967a42f52fa2885ff4d71120edc95c2422199f
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-02-07 14:08:16 +0900

    Title: 벌금은 누구에게, Time: 140ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240207/벌금은 누구에게/who-will-pay.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] Student = new int[n + 1];
+        int ans = 0;
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int num = Integer.parseInt(st.nextToken());
+            Student[num]++;
+
+            if(Student[num] == k) ans = num;
+        }
+
+        System.out.print(ans == 0 ? -1 : ans);
+    }
+}
