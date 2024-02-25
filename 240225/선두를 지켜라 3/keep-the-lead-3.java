dce5751610c74ebe4ac921698415594f4b8d3907
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-02-25 16:07:10 +0900

    Title: 선두를 지켜라 3, Time: 209ms, Memory: 89MB, Status: Passed - CodeTree

---
+++ 240225/선두를 지켜라 3/keep-the-lead-3.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        //a = 0, b = 1, c = 2.
+        int winner = -1;
+        int cnt = 0;
+        int time = 1;
+        
+        int[] a = new int[10000001];
+        int[] b = new int[10000001];
+
+        int aT = 0;
+        int bT = 0;
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int v = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+
+            time += t;
+
+            for(int j = 0; j < t; j++) {
+                a[aT + 1] = a[aT] + v;
+                aT++;
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int v = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+
+            time += t;
+
+            for(int j = 0; j < t; j++) {
+                b[bT + 1] = b[bT] + v;
+                bT++;
+            }
+        }
+
+        for(int i = 1; i <= time; i++) {
+            if(a[i] > b[i]) {
+                if(winner != 0) cnt++;
+                winner = 0;
+            }
+            else if(a[i] < b[i]) {
+                if(winner != 1) cnt++;
+                winner = 1;
+            }
+            else{
+                if(winner != 2) cnt++;
+                winner = 2;
+            }
+        }
+
+        System.out.print(cnt - 1);
+    }
+}
