3dafe00cf89c25c66ef9a37061f2ffb71477e3b7
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-03-05 17:58:37 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

--- 240305/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
+++ 240305/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -24,19 +24,16 @@ public class Main {
         //1번, n번 빼고 한 번씩 스킵할 반복문1.
         for(int i = 1; i < n - 1; i++) {
             int sum = 0;
-            int CurrX = listX.get(i);
-            int Curry = listY.get(i);
-            listX.remove(i);
-            listY.remove(i);
-            for(int j = 0; j < n - 2; j++) {
+            int prevIdx = 0;
+            for(int j = 1; j < n; j++) {
             //1번과 n번 빼고 스킵
-            int disX = Math.abs(listX.get(j) - listX.get(j + 1));
-            int disY = Math.abs(listY.get(j) - listY.get(j + 1));
+            if(i == j) continue;
+            int disX = Math.abs(listX.get(prevIdx) - listX.get(j));
+            int disY = Math.abs(listY.get(prevIdx) - listY.get(j));
             sum += disX + disY;
+            prevIdx = j;
             }
             if(Mindis > sum) Mindis = sum;
-            listX.add(i, CurrX);
-            listY.add(i, Curry);
         }
         System.out.print(Mindis);
     }
