7e99621f5f9715f85b79a550f6eabe508905750f
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-03-07 23:55:30 +0900

    Title: 최고의 13위치 2, Time: 109ms, Memory: 9MB, Status: Passed - CodeTree

--- 240307/최고의 13위치 2/best-place-of-13-2.java
+++ 240307/최고의 13위치 2/best-place-of-13-2.java
@@ -1,43 +1,38 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
     static final int INT_MIN = Integer.MIN_VALUE;
     static int n = 0;
-
-    public static void main(String[] args) throws IOException {
+    static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
         int[][] arr = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            for(int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         int Max = INT_MIN;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - 2; j++)
+            for(int k = 0; k < n; k++)
+            for(int l = 0; l < n - 2; l++) {
+                if(i == k && Math.abs(j - l) <= 2) continue;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j + 2 < n) { // 수평 방향으로 3칸을 이동할 수 있는 경우에만 진행
-                    int cnt = 0;
-                    // 첫 번째 격자
-                    for (int k = j; k <= j + 2; k++) {
-                        if (arr[i][k] == 1) cnt++;
-                    }
-                    // 두 번째 격자
-                    for (int k = j; k <= j + 2; k++) {
-                        if (i + 1 < n && arr[i + 1][k] == 1) cnt++;
-                    }
-                    Max = Math.max(Max, cnt);
-                }
+                int cnt1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                int cnt2 = arr[k][l] + arr[k][l + 1] + arr[k][l + 2];
+
+                Max = Math.max(cnt1 + cnt2, Max);
             }
         }
-
         System.out.println(Max);
     }
 }
