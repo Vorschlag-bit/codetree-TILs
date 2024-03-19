60f80bc50c62dfa6e6fe408299144b413a9ec818
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-03-19 22:46:08 +0900

    Title: 바구니 안의 사탕 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240319/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static final int R = 100;
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[R];
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int candy = Integer.parseInt(st.nextToken());
+            int pos = Integer.parseInt(st.nextToken());
+
+            arr[pos] += candy;
+        }
+        int Max = 0;
+        for(int i = k; i < R - k; i++) {//중심점 찾는 반복문.
+            int sum = 0;
+            for(int j = i - k; j <= i + k; j++) {
+                sum += arr[j];
+            }
+            Max = Math.max(sum, Max);
+        }
+        System.out.println(Max);
+    }
+}
