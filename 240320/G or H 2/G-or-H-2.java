5d34e0c726fc4b564d5721c20e5224f4b96fa999
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-03-20 12:40:08 +0900

    Title: G or H 2, Time: 131ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240320/G or H 2/G-or-H-2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[101];
+
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int pos = Integer.parseInt(st.nextToken());
+            char a = st.nextToken().charAt(0);
+
+            if(a == 'G') arr[pos] = 1;
+            else arr[pos] = 2;
+        }
+
+        int ans = 0;
+        int Max = 0;
+
+        for(int i = 0; i <= 100; i++) {
+            int Hcnt = 0, Gcnt = 0;
+            int size = 0;
+            for(int j = i; j <= 100; j++) {
+                if(arr[j] != 0) {
+                    if(arr[j] == 1) Gcnt++;
+                    else Hcnt++;
+
+                    if(Hcnt == Gcnt || (Hcnt != 0 && Gcnt == 0) || (Hcnt == 0 && Gcnt != 0)) {
+                        size = j - i;
+                        Max = Math.max(size, Max);
+                    }
+                }
+            }
+            ans = Math.max(ans, size);
+        }
+        if(n == 1) System.out.println(0);
+        else System.out.println(ans - 1);
+    }
+}
