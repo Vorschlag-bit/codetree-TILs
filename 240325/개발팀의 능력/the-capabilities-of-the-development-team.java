580ba4783e1fbd3a663f1c577cf85846b711c283
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-03-25 00:43:08 +0900

    Title: 개발팀의 능력, Time: 101ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240325/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[5];
+        int total = 0;
+
+        for(int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            total += arr[i];
+        }
+
+        int minddiff = Integer.MAX_VALUE;
+
+        for(int i = 0; i < 5; i++)
+        for(int j = i + 1; j < 5; j++) {
+            int sum1 = arr[i] = arr[j];
+            for(int a = 0; a < 5; a++)
+            for(int b = a + 1; b < 5; b++) {
+                if(a == i || a == j || b == i || b == j)
+                continue;
+
+                int sum2 = arr[a] + arr[b];
+                int sum3 = total;
+                sum3 -= (sum2 + sum1);
+
+                if(sum1 == sum2 || sum2 == sum3 || sum1 == sum3)
+                continue;
+
+                int max = Math.max(sum1, Math.max(sum2, sum3));
+                int min = Math.min(sum1, Math.min(sum2, sum3));
+
+                minddiff = Math.min(minddiff, max - min);
+            }
+        }
+        if(minddiff == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(minddiff);
+    }
+}
