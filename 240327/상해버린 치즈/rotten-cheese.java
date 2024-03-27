33e9d3dc3b984cf788cb8e0737496f4d3e608dc2
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-03-27 23:48:25 +0900

    Title: 상해버린 치즈, Time: 118ms, Memory: 9MB, Status: Passed - CodeTree

--- 240327/상해버린 치즈/rotten-cheese.java
+++ 240327/상해버린 치즈/rotten-cheese.java
@@ -16,6 +16,7 @@ public class Main {
         int[] time = new int[d];
         int[] sick = new int[s];
         int[] sickt = new int[s];
+        int[] candidate = new int[m + 1];
 
         for(int i = 0; i < d; i ++) {
             st = new StringTokenizer(br.readLine());
@@ -37,31 +38,28 @@ public class Main {
             //병의 기록을 보고 상한 치즈 후보군를 찾는 반복문.
             int sickperson = sick[i];
             int paintime = sickt[i];
-            int candidate = 0;
-            boolean check = true; //현 상한 후보군을 먹은 사람이 이전 후보군을 먹은 기록이 있는지 확인
             for(int j = 0; j < d; j++) {
                 if(sickperson == person[j] && time[j] < paintime)
-                candidate = ch[j];
-                int[] alreadyeat = new int[n + 1]; //후보군이 2개 이상일 경우, 최대의 약보유를 찾아야 함.
-                int med = 0;
-                //상한 치즈인지 확인하는 반복문.
-                for(int k = 0; k < d; k++) {
-                    //현 상한 후보군을 먹은 사람이 이전 후보군을 먹은 기록이 있는지 체크.
-                    if(sickperson == person[k] && cheez == ch[k]) {
-                        check = false;
-                        break;
-                    }
-                    //이미 상치 먹은 사람이 2개 이상 먹는지 확인.
-                    if((candidate == ch[k]) && alreadyeat[person[k]] == 0) {
-                        alreadyeat[person[k]]++;
-                        med++;
-                    }
-                }
-                if(check)
-                ans = Math.max(med, ans);
+                candidate[ch[j]]++;
             }
-            if(check)
-            cheez = candidate;
+        }
+        int Maxcnt = 0;
+        for(int i = 1; i <= m; i++)
+        Maxcnt = Math.max(Maxcnt, candidate[i]);
+        //가장 많이 중복된 치즈 == 상한 치즈.
+        for(int j = 1; j <= m; j++){
+            if(candidate[j] >= Maxcnt)
+            cheez = j;
+
+            int[] alreadyEat = new int[n + 1];
+            int med = 0;
+            for(int k = 0; k < d; k++) {
+            if(ch[k] == cheez && alreadyEat[person[k]] == 0) {
+                alreadyEat[person[k]]++;
+                med++;
+            }
+        }
+        ans = Math.max(ans, med);
         }
         System.out.println(ans);
     }
