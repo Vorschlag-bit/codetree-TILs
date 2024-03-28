174d57a3d628bbb13d3983e434cff54bc603c134
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-03-28 17:30:56 +0900

    Title: 이상한 폭탄 2, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240328/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            int boom = arr[i];
+            boolean Check = false;
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+
+                if(boom == arr[j] && Math.abs(i - j) <= k)
+                ans = Math.max(ans, arr[i]);
+            }
+        }
+        System.out.println(ans);
+    }
+}
