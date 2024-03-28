6061c5bfb3a4bab5a4b77619d850cbabfc417219
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-03-28 17:31:39 +0900

    Title: 이상한 폭탄 2, Time: 120ms, Memory: 8MB, Status: Passed - CodeTree

--- 240328/이상한 폭탄 2/strange-bomb-2.java
+++ 240328/이상한 폭탄 2/strange-bomb-2.java
@@ -15,7 +15,7 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int ans = 0;
+        int ans = -1;
 
         for(int i = 0; i < n; i++) {
             int boom = arr[i];
