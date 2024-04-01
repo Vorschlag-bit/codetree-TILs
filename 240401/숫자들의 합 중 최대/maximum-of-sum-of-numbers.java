b52994dee78c6d1c781971b62ff5d6cfac41d455
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-04-01 12:11:07 +0900

    Title: 숫자들의 합 중 최대, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240401/숫자들의 합 중 최대/maximum-of-sum-of-numbers.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+
+        for(int i = x; i <= y; i++) {
+            int a = i % 10;
+            int b = (i / 10) % 10;
+            int c = (i / 100) % 10;
+            int d = (i / 1000) % 10;
+            int e = i / 10000;
+
+            int Current = a + b + c + d + e;
+            ans = Math.max(Current, ans);
+        }
+        System.out.println(ans);
+    }
+}
