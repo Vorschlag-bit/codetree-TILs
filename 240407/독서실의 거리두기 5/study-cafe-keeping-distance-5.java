768e63e4eea5c409d80ed4adf5113b1f2889f099
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-04-07 22:50:19 +0900

    Title: 독서실의 거리두기 5, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

--- 240407/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
+++ 240407/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static int n;
@@ -21,38 +20,52 @@ public class Main {
         int ans = 0;
         for (int i = 0; i < n; i++) {
             // i부터 인덱스로 배열을 돌면서 탐색.
-            if (arr[i] == 1)
+            int[] temp = new int[n];
+            for(int j = 0; j < n; j++)
+                temp[j] = arr[j]; // arr 배열을 temp 배열로 복사
+            if (temp[i] == 1)
                 continue;
-            int dis1 = getleft(i, arr);
-            int dis2 = getrigth(i, arr);
 
-            int Max = Math.min(dis1, dis2);
-            ans = Math.max(Max, ans);
+            temp[i] = 1;
+
+            int minDistance = Integer.MAX_VALUE; // 최소 거리를 구하기 위한 변수 초기화
+
+            for(int j = 0; j < n; j++) {
+                if(temp[j] == 1) { // 1이면 거리 계산
+                    int dis1 = getleft(j, temp);
+                    int dis2 = getrigth(j, temp);
+                    int minDis = Math.min(dis1, dis2); // 왼쪽과 오른쪽 거리 중 최소값
+                    minDistance = Math.min(minDistance, minDis); // 현재까지의 최소 거리 업데이트
+                }
+            }
+
+            // 최소 거리가 이전에 구한 최대 거리보다 크면 업데이트
+            ans = Math.max(minDistance, ans);
         }
         System.out.print(ans);
     }
 
-    private static int getleft(int idx, int[] arr) {
+    // 왼쪽 거리를 구하는 메서드
+    private static int getleft(int idx, int[] temp) {
         int dis = Integer.MAX_VALUE;
         for (int i = idx - 1; i >= 0; i--) {
-            if (arr[i] == 1) {
+            if (temp[i] == 1) {
                 dis = idx - i;
                 break;
             }
         }
-
         return dis;
     }
 
-    private static int getrigth(int idx, int[] arr) {
-      int dis = Integer.MAX_VALUE;
+    // 오른쪽 거리를 구하는 메서드
+    private static int getrigth(int idx, int[] temp) {
+        int dis = Integer.MAX_VALUE;
         for (int i = idx + 1; i < n; i++) {
-            if (arr[i] == 1) {
+            if (temp[i] == 1) {
                 dis = i - idx;
                 break;
             }
         }
-
         return dis;
     }
 }
