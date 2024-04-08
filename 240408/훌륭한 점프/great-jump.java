9ecddb6d4bfc37529d83ecb3ef4dd6870a202644
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-04-08 18:17:51 +0900

    Title: 훌륭한 점프, Time: 107ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240408/훌륭한 점프/great-jump.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n;
+    static int k;
+    static int[] arr;
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        
+        st = new StringTokenizer(br.readLine());
+        int Max = 0;
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            Max = Math.max(Max, arr[i]);
+        }
+
+        int ans = Max;
+        for(int i = Max; i >= 1; i--) {
+            if(Check(i))
+            ans = Math.min(ans, i);
+        }
+        System.out.println(ans);
+    }
+    private static boolean Check(int Maxv) {
+        int cnt = 0;
+
+        int[] instep = new int[n];
+        for(int i = 0; i < n; i++) {
+            if(arr[i] <= Maxv)
+            instep[cnt++] = i;
+        }
+        if(cnt == 1) return false;
+
+        for(int i = 1; i < cnt; i++) {
+            int dis = instep[i] - instep[i - 1];
+            if(dis > k)
+            return false;
+        }
+
+        return true;
+    }
+}
