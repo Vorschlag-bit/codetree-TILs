1c71b3d383ecc737cd3b1996d22d35e041c26f94
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-04-09 00:50:45 +0900

    Title: 초기 수열 복원하기, Time: 199ms, Memory: 15MB, Status: Passed - CodeTree

--- 240409/초기 수열 복원하기/restore-initial-sequence.java
+++ 240409/초기 수열 복원하기/restore-initial-sequence.java
@@ -14,32 +14,35 @@ public class Main {
         for(int i = 0; i < n - 1; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int fir = arr[0] - 1;
-        for(int i = 1; i < 1000; i++) {
+
+        for(int i = 1; i <= n; i++) {
             //첫 번째 arr[0] = i라고 가정하기
             if(i > arr[0]) continue;
 
             int[] arr2 = new int[n];
             arr2[0] = i;
-            int next = arr[0] - i;
-            boolean Check = true;
-            for(int j = 1; j < n - 1; j++) {
-                int temp = next;
-                arr2[j] = temp;
-                next = arr[j] - temp;
-                if(temp == next || next <= 0 || next > n) {
-                    Check = false;
-                    break;
-                }
-                if(next > n) Check = false;
+
+            for(int j = 1; j < n; j++) {
+                arr2[j] = arr[j - 1] - arr2[j - 1];
             }
 
-            if(Check) {
-                arr2[n - 1] = next;
-                for(int a = 0; a < n; a++)
-                System.out.print(arr2[a] + " ");
-                break;
+            boolean Check = true;
+            boolean[] exist = new boolean[1001];
+            for(int j = 0; j < n; j++) {
+                if(arr2[j] <= 0 || arr2[j] > n)
+                Check = false;
+
+                else {
+                    if(exist[arr2[j]])
+                    Check = false;
+
+                    exist[arr2[j]] = true;
+                }
             }
+
+            if(Check)
+            for(int j = 0; j < n; j++)
+            System.out.print(arr2[j] + " ");
         }
     }
 }
