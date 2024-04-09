e26059bb9caa5254601581082599d4572cce6eff
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-04-09 23:59:24 +0900

    Title: 언덕 깎기, Time: 181ms, Memory: 12MB, Status: Passed - CodeTree

--- 240409/언덕 깎기/hill-cutting.java
+++ 240409/언덕 깎기/hill-cutting.java
@@ -1,51 +1,30 @@
-import java.util.Arrays;
-import java.util.Scanner;
 import java.io.*;
-
+import java.util.*;
 public class Main {
-    public static void main(String[] args)throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
 
-        int n = Integer.parseInt(br.readLine());
+        int n = sc.nextInt();
 
-        int[] hills = new int[n]; // 언덕의 높이 배열
+        int[] hills = new int[n];
 
         for(int i = 0; i < n; i++)
-        hills[i] = Integer.parseInt(br.readLine());
+        hills[i] = sc.nextInt();
 
-        int minCost = findMinCost(hills);
-        System.out.println(minCost);
-    }
+        int ans = Integer.MAX_VALUE;
 
-    public static int findMinCost(int[] hills) {
-        Arrays.sort(hills); // 언덕의 높이를 오름차순으로 정렬
-
-        int n = hills.length;
-        int minHeight = hills[0]; // 가장 낮은 언덕의 높이
-        int maxHeight = hills[n - 1]; // 가장 높은 언덕의 높이
-
-        int minCost = Integer.MAX_VALUE;
-
-        // 가장 높은 언덕과 가장 낮은 언덕의 높이 차이를 17 이하로 만들기 위한 모든 경우를 탐색
-        for (int i = minHeight; i <= maxHeight - 17; i++) {
-            int currentMaxHeight = i + 17; // 현재 높이로부터 17만큼 높은 언덕의 높이
+        for(int h = 1; h <= 100; h++) {
+            int maxh = h + 17;
             int cost = 0;
-
-            // 각 언덕의 높이를 현재 높이로 조절하거나 높이 차이가 17 이하로 만들기 위해 조절한 비용 계산
-            for (int j = 0; j < n; j++) {
-                if (hills[j] > currentMaxHeight) {
-                    int heightDiff = Math.abs(hills[j] - currentMaxHeight);
-                    cost += heightDiff * heightDiff;
-                }
-                if(hills[j] < i) {
-                    int diff = i - hills[j];
-                    cost += diff * diff;
-                }
+            for(int i = 0; i < n; i++) {
+                if(hills[i] < h)
+                cost += (h - hills[i]) * (h - hills[i]);
+                if(hills[i] > maxh)
+                cost += (hills[i] - maxh) * (hills[i] - maxh);
             }
-
-            minCost = Math.min(minCost, cost);
+            ans = Math.min(cost, ans);
         }
-
-        return minCost;
+        System.out.print(ans);
     }
 }
