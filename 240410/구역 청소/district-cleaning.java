568ae02979d27c73608ca928584c5ee33989f369
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-04-10 19:51:21 +0900

    Title: 구역 청소, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

--- 240410/구역 청소/district-cleaning.java
+++ 240410/구역 청소/district-cleaning.java
@@ -1,12 +1,10 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args)throws IOException {
-        // 여기에 코드를 작성해주세요.
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int[] arr = new int[101];
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
@@ -17,18 +15,18 @@ public class Main {
         int d = Integer.parseInt(st.nextToken());
 
         int ans = 0;
+        int[] arr = new int[101];
+        for(int i = a; i <= b; i++)
+        arr[i]++;
 
-        //겹칠 경우
-        if(c <= a && a <= d || c <= b && b <= d)
-        ans = Math.max(d - a, b - c);
+        for(int i = c; i <= d; i++)
+        arr[i]++;
 
-        //포함되는 경우
-        if(c <= a && b <= d || a <= c && d <= b)
-        ans = Math.max(d - c, b - a);
-
-        //안 겹칠 경우
-        else ans = (d - c) + (b - a);
-
-        System.out.println(ans);
+        for(int i = 0; i <= 100; i++)
+        if(arr[i] > 0) ans++;
+        //겹치지 않는 경우만 파악
+        if(c > b || a > d)
+        System.out.println(ans - 2);
+        else System.out.print(ans - 1);
     }
 }
