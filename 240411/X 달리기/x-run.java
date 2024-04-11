bd542da9132792245933f55da7ce6c20e9abc399
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-04-11 23:44:21 +0900

    Title: X 달리기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240411/X 달리기/x-run.java
+++ 240411/X 달리기/x-run.java
@@ -2,27 +2,31 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         
-        int time = 1;
+        int time = 0;
         int speed = 1;
        
-       while(x > 0) {
-         if(speed < 1) break;
-        time++;
-        x -= speed;
-        //속도를 1높일 경우 필요한 최소 거리
-        int mindis = (speed + 2) * (speed + 1)/2;
-        if(mindis < x) {
-            speed++;
+        while (true) {
+            time++;
+            x -= speed;
+
+            if(x == 0) break;
+            
+            // 현재 속도에서 1초 후의 거리를 계산하여 최소 거리를 구함
+            int nextMinDistance = (speed + 1) * (speed + 2) / 2;
+            
+            // 남은 거리가 최소 거리보다 크면 속도를 증가시킴
+            if (x >= nextMinDistance) {
+                speed++;
+            }
+            else if(x >= speed * (speed + 1) / 2){
+                //아무것도 안 함
+            }
+            // 그 외의 경우에는 속도를 감소시킴
+            else speed--;
         }
-        //감속
-        if(mindis > x) {
-            speed--;
-        }
-       }
-       System.out.print(time);
+        System.out.print(time);
     }
 }
