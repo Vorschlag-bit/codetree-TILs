f039188e75608ab96373574d5edd6e6b45b0bdef
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-05-28 13:20:43 +0900

    Title: L, R 그리고 B, Time: 107ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240528/L, R 그리고 B/l-r-and-b.java
+++ 240528/L, R 그리고 B/l-r-and-b.java
@@ -9,6 +9,7 @@ public class Main {
         
         int Bi = 0, Bj = 0;
         int Li = 0, Lj = 0;
+        int Ri = 0, Rj = 0;
         for(int i = 0; i < 10; i++) {
             String str = br.readLine();
             for(int j = 0; j < 10; j++) {
@@ -19,10 +20,14 @@ public class Main {
                 if(arr[i][j] == 'L') {
                     Li = i; Lj = j;
                 }
+                if(arr[i][j] == 'R') {
+                    Ri = i; Rj = j;
+                }
             }
         }
         int dis = Math.abs(Bi - Li) + Math.abs(Bj - Lj) - 1;
-        if(Bi == Li || Bj == Lj) System.out.print(dis + 2);
+        if(((Bi == Li)&&(Li == Ri) || (Bj == Lj)&&(Lj == Rj)))
+         System.out.print(dis + 2);
         else
         System.out.print(dis);
     }
