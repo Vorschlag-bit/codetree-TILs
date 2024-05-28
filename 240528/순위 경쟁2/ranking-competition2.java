6ee1182792c9481982810bce39a21b4f315e7865
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-05-28 10:09:21 +0900

    Title: 순위 경쟁2, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

--- 240528/순위 경쟁2/ranking-competition2.java
+++ 240528/순위 경쟁2/ranking-competition2.java
@@ -1,37 +1,34 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args)throws IOException {
-        // 여기에 코드를 작성해주세요.
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
         int cA = 0, cB = 0;
         int lA = 0, lB = 0;
-        int ans = 1;
+        int ans = 0;
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             String c = st.nextToken();
             int s = Integer.parseInt(st.nextToken());
-            //점수 입력
-            if(c == "A") cA = lA + s;
+            // 점수 입력
+            if (c.equals("A")) cA = lA + s;
             else cB = lB + s;
 
-            if((lA >= lB) && cA < cB)
-            ans++;
-            else if((lA <= lB) && cA > cB)
-            ans++;
-            else if(lA == lB && (cA > cB || cA < cB))
-            ans++;
-            else if((lA > lB || lA < lB) && cA == cB)
-            ans++;
+            // 점수가 바뀌는 경우와 동점이 되는 경우를 모두 처리
+            if ((lA >= lB && cA < cB) || (lA <= lB && cA > cB) || (lA != lB && cA == cB)) {
+                ans++;
+            }
 
             lA = cA;
             lB = cB;
         }
+        // 최종 결과 출력
         System.out.println(ans);
     }
 }
