9d2393ac4866fe7b2fcd9badd8c02e27e28cbe6f
Vorschlag-bit <147306997+Vorschlag-bit@users.noreply.github.com>
2024-06-01 23:17:54 +0900

    Title: 홀수 짝수의 묶음, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240601/홀수 짝수의 묶음/odd-even-bundle.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args)throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        int odd = 0, even = 0;
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if(arr[i] % 2 == 0) even++;
+            else odd++;
+        }
+
+        int ans = 0;
+        if(even > odd)
+        ans = even + odd;
+        else if(even == 0) {
+            if(odd % 3 == 0) ans = (odd / 3) * 2;
+
+            else if(odd % 3 == 1 && odd == 7)
+            ans = 3;
+            else if(odd % 3 == 1 && odd > 7)
+            ans = ((odd - 7) / 3) * 2 + 3;
+
+        }
+        else if(odd / 3 == 0) {
+            ans = (odd / 3) * 2 + 1;
+        }
+        else if(odd % 3 == 2) {
+            if(even % 2 == 0)
+            ans = (odd / 3) * 2 + 2;
+            else ans = (odd / 3) * 2 + 1;
+        }
+        else ans = (odd / 3) * 2 + 1;
+
+        System.out.print(ans);
+    }
+}
